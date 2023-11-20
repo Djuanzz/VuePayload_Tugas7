06165ad023c09cf0af7c8cd8792dfe471afbf60b
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2023-11-20 15:42:57 +0700

    initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import payload from "payload";
+import cors from "cors";
+
+require("dotenv").config();
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+  })
+);
+
+// Redirect root to Admin panel
+app.get("/", (_, res) => {
+  res.redirect("/admin");
+});
+
+const start = async () => {
+  // Initialize Payload
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    express: app,
+    onInit: async () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+    },
+  });
+
+  // Add your own express routes here
+  app.listen(process.env.PORT, () => {
+    payload.logger.info(`Server running on port ${process.env.PORT}/admin`);
+  });
+};
+
+start();
